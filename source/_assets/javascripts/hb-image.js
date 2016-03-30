ee2cf8c66b1ce2265839fda4fa8dfc3805a2bf5c
Tuomas Wolkowski <tuomas.wolkowski@gmail.com>
2016-03-29 20:19:38 -0600

    Checks in the build

--- source/_assets/javascripts/hb-image.js
+++
@@ -1,8 +0,0 @@
-var img = document.getElementById("hb-article-content");
-img.addEventListener('click', function () {
-  if (img.height != 200) {
-    img.height = 200;
-  } else {
-    img.height = -1;
-  }
-});
